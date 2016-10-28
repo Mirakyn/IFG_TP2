5c31b00078aaa65ec6aa0107a3d6dcfd8d71683e
Kurond <charlymoreau30@gmail.com>
2016-10-28 18:21:34 -0400

    Ajout de Groupe et Liste_Courses

--- Projet/src/model/Groupe.java
+++ Projet/src/model/Groupe.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Groupe {
-
+	private int id;
+	private String nom;
+	
+	public Groupe(int id, String nom)
+	{
+		this.id = id;
+		this.nom = nom;
+	}
+	
+	public int getID()
+	{
+		return this.id;
+	}
+	
+	public String getNom()
+	{
+		return this.nom;
+	}
 }
--- Projet/src/model/Liste_Courses.java
+++ Projet/src/model/Liste_Courses.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Liste_Courses {
-
+	private int id;
+	private int idGroupe;
+	private String nom;
+	
+	public Liste_Courses(int id, int idGroupe, String nom)
+	{
+		this.id = id;
+		this.idGroupe = idGroupe;
+		this.nom = nom;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public int getIDgroupe() {
+		return this.idGroupe;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
 }
